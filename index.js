b8f9c91b7b4b9f09d0eed4e84015309ad2d14c37
AtymTima <timanis188@gmail.com>
2019-12-15 00:08:23 +0600

    updated full first page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const selectElement = (element) =>
 {
 	return document.querySelector(element);
-}
+};
 
 const menuToggler = selectElement('.menu-toggle');
 const body = selectElement('body');
@@ -9,4 +9,14 @@ const body = selectElement('body');
 menuToggler.addEventListener('click', () =>
 {
 	body.classList.toggle('open');
-})
+});
+
+ScrollReveal().reveal('.scroll', 
+{ 
+	delay: 100, 
+	easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    distance: '30px',
+    duration: 1000,
+    desktop: true,
+    mobile: true,
+});
